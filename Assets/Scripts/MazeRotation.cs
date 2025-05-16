44da58307ea74e665be9e6d353e9b532d685d606
ilhamhafidz404 <xxspanzie@gmail.com>
2025-05-16 11:07:05 +0700

    Maze Rotation

---
+++ Assets/Scripts/MazeRotation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeRotation : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ilham Jago nemen cok!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, 150 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -150 * Time.deltaTime);
+        }
+    }
+}
